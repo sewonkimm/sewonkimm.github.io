73ec72f41f41831ff3c732e5baf41a04a852554a
SeWonKim <swon962@gmail.com>
2022-09-12 00:00:47 +0900

    fix: 🐛 blog 전체 목록에서 댓글 보이는 버그 해결

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,16 +2,22 @@ import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
 import { DiscussionEmbed } from "disqus-react";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
+import { useLocation } from "@docusaurus/router";
 
 export default function BlogPostItemWrapper(props) {
   const { metadata } = useBlogPost();
+  const location = useLocation();
+
   const { frontMatter, slug, title } = metadata;
   const { comments = true } = frontMatter;
+  const { pathname } = location;
+  const showComments = comments && pathname !== "/";
 
   return (
     <>
       <BlogPostItem {...props} />
-      {comments && (
+      <h1>{comments}</h1>
+      {showComments && (
         <DiscussionEmbed
           shortname="wonolog"
           config={{
