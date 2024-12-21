b4de3a56a9dca25b7722d7002eb477e3d5b07df9
SeWonKim <swon962@gmail.com>
2024-12-21 11:22:10 +0900

    feat: 🎸 Add width, height props

--- src/components/ImageLayout.tsx
+++ src/components/ImageLayout.tsx
@@ -4,11 +4,13 @@ import "./ImageLayout.css";
 type Props = {
   src: string;
   description?: string;
+  width?: number;
+  height?: number;
 };
-const ImageLayout = ({ src, description }: Props) => {
+const ImageLayout = ({ src, description, width, height }: Props) => {
   return (
     <div className="root">
-      <img src={src} alt={description || ""} />
+      <img src={src} alt={description || ""} width={width} height={height} />
       {description && <p className="description">{description}</p>}
     </div>
   );
